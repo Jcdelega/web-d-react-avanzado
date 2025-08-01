95865fb65c8523de9a9c6a4cd800f0a9ad51b2ae
Jose Meza <jcdlega@gmail.com>
2025-07-31 18:52:28 -0600

    CI: Backend

--- express/src/app.js
+++ express/src/app.js
@@ -1,62 +1,23 @@
 import 'dotenv/config.js'
 import musicData from './music.js'
 import express from 'express'
+import fs from 'fs'
 
 const app = express()
 
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Music API! Try /api/music/rock or /api/music/rock?order=title')
-})
+const readData = (filePath) => {
+  try {
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    return data
+  } catch (error) {
+    console.log(`Error reading or parsing the path ${filePath}: ${error}`)
+  }
+}
 
 app.get('/api/music', (req, res) => {
-  res.send(musicData)
-})
-
-app.get('/api/music/genre/:genre', (req, res) => {
-  const genre = req.params.genre
-  res.send(musicData[genre])
-})
-app.get('/api/music/rock', (req, res) => {
-  if (req.query.order === 'title') {
-    const orderedSongs = musicData.rock.sort((a, b) => a.title - b.title)
-    return res.send(orderedSongs)
-  }
-  res.send(musicData.rock)
-})
-app.get('/api/music/rock/:year/:country', (req, res) => {
-  const { year, country } = req.params
-  const filteredResults = musicData.rock.filter(song => Number(year) === song.year || country === song.country)
-  if (filteredResults === 0) return `There is no results that matching with this: ${year} or this country: ${country}`
-  res.send(filteredResults)
-})
-app.get('/api/music/hip_hop', (req, res) => {
-  res.send(musicData.hip_hop)
-})
-app.get('/api/music/pop', (req, res) => {
-  res.send(musicData.pop)
-})
-app.get('/api/music/electronic', (req, res) => {
-  res.send(musicData.electronic)
-})
-app.get('/api/music/r_and_b_soul', (req, res) => {
-  res.send(musicData.r_and_b_soul)
-})
-app.get('/api/music/latin_music', (req, res) => {
-  res.send(musicData.latin_music)
-})
-
-app.use(express.json())
-
-app.post('/api/music', (req, res) => {
-  const newSong = req.body
-
-  console.log(newSong)
-  res.status(201).send({
-    mensaje: 'Nwe song received successfully',
-    datos: newSong
-  })
+  res.send(readData('./src/db.json'))
 })
 
 app.listen(PORT, () => {
